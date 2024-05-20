7a64855e3e3b2bf9bee817c25f1c6337ff87f74a
injae-348 <ant0593@naver.com>
2024-05-20 20:20:24 +0900

    test

---
+++ BuddyBridge_BE/src/main/java/com/example/buddybridge_be/BuddyBridgeBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.buddybridge_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyBridgeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuddyBridgeBeApplication.class, args);
+    }
+
+}
